97bf5e1afca7cedde5f68516e97dc3fff8d124ed
Carlos <ficachi048@gmail.com>
2025-05-09 17:34:44 -0600

    read me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gestion-citas
+
+en mysql crear usuario=test_kosmos
+con su password=123456
+
+
+importar la bd que esta en la carpeta bd dentro del repositorio
---
+++ bd/gestion_citas_citas.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: gestion_citas
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `citas`
+--
+
+DROP TABLE IF EXISTS `citas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `citas` (
+  `id_cita` int NOT NULL AUTO_INCREMENT,
+  `id_consultorio` int NOT NULL,
+  `id_doctor` int NOT NULL,
+  `horario_consulta` datetime NOT NULL,
+  `nombre_paciente` varchar(60) NOT NULL,
+  `cancelada` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id_cita`),
+  KEY `citas_consultorios_idx` (`id_consultorio`),
+  KEY `citas_doctor_idx` (`id_doctor`),
+  CONSTRAINT `citas_consultorios` FOREIGN KEY (`id_consultorio`) REFERENCES `consultorios` (`id_consultorio`),
+  CONSTRAINT `citas_doctor` FOREIGN KEY (`id_doctor`) REFERENCES `doctores` (`id_doctor`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citas`
+--
+
+LOCK TABLES `citas` WRITE;
+/*!40000 ALTER TABLE `citas` DISABLE KEYS */;
+INSERT INTO `citas` VALUES (1,1,1,'2023-12-14 22:43:18','Rafael Dos Anjos',NULL);
+/*!40000 ALTER TABLE `citas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-09 17:32:55
